08a02dd1e8d5bb654e01b3977f0c7ce610595a4d
Jonathan Dean <Jon@mbpro.myfiosgateway.com>
2022-07-11 14:20:32 -0400

    Setup some baseline stuff for the grabber cta

--- src/components/CTA/Grabber/Grabber.tsx
+++ src/components/CTA/Grabber/Grabber.tsx
@@ -3,16 +3,64 @@ import React from "react";
 
 const Grabber = () => {
 	return (
-		<div className=" h-[285px] w-[213px] my-10 relative rounded-t-full border-3 border-red-900">
-			<Image
-				src={"/JTD.jpg"}
-				className="rounded-t-full border-4 border-red-900"
-				alt="A picture of Jonathan Dean"
-				width="213px"
-				height="285px"
-				layout="fill"
-				// objectFit="cover"
-			/>
+		<div>
+			<div className=" h-[280px] w-[213px] my-10 py-[0.25rem] px-[0.25rem] relative border-4 rounded-t-full border-red-900">
+				<Image
+					src={"/JTD.jpg"}
+					className="rounded-t-full"
+					alt="A picture of Jonathan T. X. L. Dean"
+					width="213px"
+					height="285px"
+				/>
+			</div>
+			<div>
+				<div>
+					<h2 className="text-lg">Skills</h2>
+					<ul className="text-sm">
+						<li>Fullstack</li>
+						<li>Frontend</li>
+						<li>Backend</li>
+						<li>Machine Learning</li>
+						<li>BioInformatics</li>
+						<li>Data Science</li>
+						<li>Data Architecture</li>
+						<li>DevOps</li>
+						<li>Systems Administration</li>
+						<li>Game Development</li>
+						<li>Graphic Design</li>
+						<li>3D Modeling</li>
+					</ul>
+				</div>
+				<div>
+					<h2 className="text-lg">Biography</h2>
+					<p className="text-sm">
+						I{"'"}m a passionate developer! I{"'"}m constantly learning and
+						evolving in my craft. I love to share what I learn with others.
+					</p>
+				</div>
+				<div>
+					<h2 className="text-lg">Statistics</h2>
+					<ul>
+						<li>
+							<h3>YOE</h3>
+							<p>5</p>
+						</li>
+						<li>
+							<h3>Repositories</h3>
+							<p>5</p>
+						</li>
+						<li>
+							<h3>Contracts</h3>
+							<p>10</p>
+						</li>
+						<li>
+							<h3>Commits</h3>
+							<p>5</p>
+						</li>
+						<li></li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
