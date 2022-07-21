25c19b16c8d66d0b176bb0e70a611f8263f19b5a
Jonathan Dean <Jon@mbpro.myfiosgateway.com>
2022-07-21 08:23:06 -0400

    Configured custom nextjs hoc for apollo coopt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,38 @@
+// NextJS
 import type { AppProps } from "next/app";
+// Components
 import "/styles/globals.css";
 import Navbar from "../components/General/Navbar/Navbar";
 import Footer from "../components/General/Footer/Footer";
 import Wrapper from "../components/General/Wrapper";
+// Backend API
+import {
+	ApolloProvider,
+	ApolloClient,
+	InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "http://localhost:8000/graphql",
+	cache: new InMemoryCache(),
+	connectToDevTools: true,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Navbar />
-			<Wrapper>
-				<Component {...pageProps} />
-			</Wrapper>
-			<Footer />
+			{/* Providers */}
+			<ApolloProvider client={client}>
+				{/* Top Layer */}
+				{/* Site Section */}
+				<>
+					<Navbar />
+					<Wrapper>
+						<Component {...pageProps} />
+					</Wrapper>
+					<Footer />
+				</>
+			</ApolloProvider>
 		</>
 	);
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,6 +1,8 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import ProjectCardList from "../../components/Projects/ProjectCardList";
 import ProjectHeader from "../../components/Projects/ProjectHeader";
+import ProjectCardProps from "../../lib/types/ProjectCard.props";
 // import ProjectCardProps from "../../lib/types/ProjectCard.props";
 
 const projects = (props) => {
@@ -15,8 +17,24 @@ const projects = (props) => {
 // This function gets called at build time
 export async function getStaticProps() {
 	// Call an external API endpoint to get posts
-	const res = await fetch("https://.../posts");
-	const posts = await res.json();
+	const getProjectsQuery = gql`
+		query {
+			getAllProjects {
+				id
+				title
+				projectInfo {
+					id
+					site
+					blog
+				}
+			}
+		}
+	`;
+
+	const apolloClient = initializeApollo();
+
+	// const res = await fetch("https://.../posts");
+	// const posts = await res.json();
 
 	// By returning { props: { posts } }, the Blog component
 	// will receive `posts` as a prop at build time
